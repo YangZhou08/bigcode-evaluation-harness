8a161fe8e475fad2cc5f5b87047c4183d7cc8da8
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-17 06:58:39 -0400

    humaneval

--- runningscript.sh
+++ runningscript.sh
@@ -1,19 +1,19 @@
 accelerate launch --num_processes 4 main.py \
   --model meta-llama/Meta-Llama-3-8B-Instruct \
   --limit 50 \
-  --tasks mbpp \
+  --tasks humaneval \
   --do_sample False \
   --n_samples 1 \
   --batch_size 1 \
   --max_length_generation 2048 \
   --allow_code_execution
 
-accelerate launch --num_processes 4 main.py \
-  --model meta-llama/Meta-Llama-3-8B \
-  --limit 50 \
-  --tasks mbpp \
-  --do_sample False \
-  --n_samples 1 \
-  --batch_size 1 \
-  --max_length_generation 2048 \
-  --allow_code_execution
+# accelerate launch --num_processes 4 main.py \
+#   --model meta-llama/Meta-Llama-3-8B \
+#   --limit 50 \
+#   --tasks mbpp \
+#   --do_sample False \
+#   --n_samples 1 \
+#   --batch_size 1 \
+#   --max_length_generation 2048 \
+#   --allow_code_execution
