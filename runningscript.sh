b30dab944de082ce1e818de9fa0ff850b0ce43fb
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-18 11:56:46 -0400

    M

--- runningscript.sh
+++ runningscript.sh
@@ -5,6 +5,7 @@ accelerate launch --num_processes 4 main.py \
   --do_sample False \
   --n_samples 1 \
   --batch_size 1 \
+  --griffin \
   --max_length_generation 2048 \
   --allow_code_execution
 
