e9b8cce8936b057b1b9da086695612a6ef49c96c
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-19 04:06:20 -0400

    cats

--- runningscript.sh
+++ runningscript.sh
@@ -4,8 +4,6 @@ accelerate launch --num_processes 8 main.py \
   --do_sample False \
   --n_samples 1 \
   --batch_size 1 \
-  --check \
-  --cats \
   --max_length_generation 512 \
   --allow_code_execution
 
