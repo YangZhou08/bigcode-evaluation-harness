979d50e1c0a7e4a209b8aa15f9427f0de0a42e98
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-19 02:36:31 -0400

    following

--- runningscript.sh
+++ runningscript.sh
@@ -1,6 +1,6 @@
-accelerate launch --num_processes 2 main.py \
+accelerate launch --num_processes 1 main.py \
   --model meta-llama/Meta-Llama-3-8B-Instruct \
-  --limit 20 \
+#   --limit 20 \
   --tasks mbpp \
   --do_sample False \
   --n_samples 1 \
