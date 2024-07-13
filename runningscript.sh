62792ad37b5a529b922405849404b9676ad16b37
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-13 17:43:21 -0400

    runningcheck

--- runningscript.sh
+++ runningscript.sh
@@ -1,4 +1,27 @@
-accelerate launch --num_processes 2 main.py \
+accelerate launch --num_processes 8 main.py \
+  --model meta-llama/Meta-Llama-3-8B-Instruct \
+  --tasks humaneval \
+  --limit 10 \
+  --do_sample False \
+  --n_samples 1 \
+  --batch_size 1 \
+  --max_length_generation 512 \
+  --enable_epatches \
+  --allow_code_execution
+
+accelerate launch --num_processes 8 main.py \
+  --model meta-llama/Meta-Llama-3-8B-Instruct \
+  --tasks humaneval \
+  --limit 10 \
+  --do_sample False \
+  --n_samples 1 \
+  --batch_size 1 \
+  --max_length_generation 512 \
+  --enable_epatches \
+  --griffin \
+  --allow_code_execution
+
+accelerate launch --num_processes 8 main.py \
   --model meta-llama/Meta-Llama-3-8B-Instruct \
   --tasks humaneval \
   --limit 10 \
