457b5ab96ef0d98eb22b097e38ae5549fb28ca8b
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-14 03:08:58 -0400

    beamnotzero

--- llama12addingtree.py
+++ llama12addingtree.py
@@ -1862,6 +1862,8 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
             
             # update generated ids, model inputs, and length for next step
             # input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1) 
+            if len(self.beam) == 0: 
+                break 
             input_ids = self.beam[0][0] 
             next_tokens = input_ids[:, -1] 
             
