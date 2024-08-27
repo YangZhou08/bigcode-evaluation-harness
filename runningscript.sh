c396a1888bc152649db92f749f8e611d8c51708f
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-08-27 19:22:36 -0400

    running

--- runningscript.sh
+++ runningscript.sh
@@ -1,25 +1,46 @@
-accelerate launch --num_processes 8 main.py \
-  --model meta-llama/Llama-2-7b-chat-hf \
-  --tasks mbppplus \
-  --do_sample False \
-  --n_samples 1 \
-  --batch_size 1 \
-  --max_length_generation 512 \
-  --enable_epatches \
-  --griffin \
-  --allow_code_execution \
-  --spr 0.5 \
-  --widthtree 10 \
-  --check \
-  --kernelsize 12 \
-  --thr 0.05 \
+# accelerate launch --num_processes 8 main.py \
+#   --model meta-llama/Llama-2-7b-chat-hf \
+#   --tasks mbppplus \
+#   --do_sample False \
+#   --n_samples 1 \
+#   --batch_size 1 \
+#   --max_length_generation 512 \
+#   --enable_epatches \
+#   --griffin \
+#   --allow_code_execution \
+#   --spr 0.5 \
+#   --widthtree 10 \
+#   --check \
+#   --kernelsize 12 \
+#   --thr 0.05 \
+
+# treesizes=(1 4 6 8) 
+# for treesize in ${treesizes[@]} 
+# do 
+# accelerate launch --num_processes 8 main.py \
+#   --model meta-llama/Llama-2-7b-hf \
+#   --tasks mbppplus \
+#   --do_sample False \
+#   --n_samples 1 \
+#   --batch_size 1 \
+#   --max_length_generation 512 \
+#   --enable_epatches \
+#   --griffin \
+#   --allow_code_execution \
+#   --spr 0.5 \
+#   --widthtree $treesize \
+#   --check \
+#   --kernelsize 12 \
+#   --thr 0.05 \
+
+# done 
 
 treesizes=(1 4 6 8) 
 for treesize in ${treesizes[@]} 
 do 
 accelerate launch --num_processes 8 main.py \
-  --model meta-llama/Llama-2-7b-hf \
-  --tasks mbppplus \
+  --model meta-llama/Meta-Llama-3-8B \
+  --tasks humaneval \
   --do_sample False \
   --n_samples 1 \
   --batch_size 1 \
@@ -30,10 +51,11 @@ accelerate launch --num_processes 8 main.py \
   --spr 0.5 \
   --widthtree $treesize \
   --check \
-  --kernelsize 12 \
+  --kernelsize 16 \
   --thr 0.05 \
 
 done 
+
 # accelerate launch --num_processes 4 main.py \
 #   --model meta-llama/Meta-Llama-3-8B \
 #   --limit 50 \
