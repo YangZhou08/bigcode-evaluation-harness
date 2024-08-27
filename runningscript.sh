ea3ebb49b4ce4e2bcd19896c94fe73accf201ed1
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-08-27 14:25:46 -0400

    mbppplus

--- runningscript.sh
+++ runningscript.sh
@@ -1,19 +1,6 @@
 accelerate launch --num_processes 4 main.py \
-  --model meta-llama/Meta-Llama-3-8B-Instruct \
-  --tasks humaneval \
-  --do_sample False \
-  --n_samples 1 \
-  --batch_size 1 \
-  --max_length_generation 512 \
-  --enable_epatches \
-  --allow_code_execution \
-
-sparsity=(0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
-for s in ${sparsity[@]};
-do
-accelerate launch --num_processes 4 main.py \
-  --model meta-llama/Meta-Llama-3-8B-Instruct \
-  --tasks humaneval \
+  --model meta-llama/Llama-2-7b-chat-hf \
+  --tasks mbppplus \
   --do_sample False \
   --n_samples 1 \
   --batch_size 1 \
@@ -21,21 +8,8 @@ accelerate launch --num_processes 4 main.py \
   --enable_epatches \
   --griffin \
   --allow_code_execution \
-  --spr ${s}
-
-accelerate launch --num_processes 4 main.py \
-  --model meta-llama/Meta-Llama-3-8B-Instruct \
-  --tasks humaneval \
-  --do_sample False \
-  --n_samples 1 \
-  --batch_size 1 \
-  --max_length_generation 512 \
-  --enable_epatches \
-  --cats \
-  --allow_code_execution \
-  --spr ${s}
-
-done
+  --spr ${s} \
+  --widthtree 1 \
 
 # accelerate launch --num_processes 4 main.py \
 #   --model meta-llama/Meta-Llama-3-8B \
