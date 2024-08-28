2e0fb936b4fe4509b650726e401c8a59e438f2a5
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-08-27 21:42:49 -0400

    trees

--- runningscript.sh
+++ runningscript.sh
@@ -14,33 +14,12 @@
 #   --kernelsize 12 \
 #   --thr 0.05 \
 
-# treesizes=(1 4 6 8) 
-# for treesize in ${treesizes[@]} 
-# do 
-# accelerate launch --num_processes 8 main.py \
-#   --model meta-llama/Llama-2-7b-hf \
-#   --tasks mbppplus \
-#   --do_sample False \
-#   --n_samples 1 \
-#   --batch_size 1 \
-#   --max_length_generation 512 \
-#   --enable_epatches \
-#   --griffin \
-#   --allow_code_execution \
-#   --spr 0.5 \
-#   --widthtree $treesize \
-#   --check \
-#   --kernelsize 12 \
-#   --thr 0.05 \
-
-# done 
-
-treesizes=(1 4 6 8) 
+treesizes=(4 6 8)
 for treesize in ${treesizes[@]} 
 do 
 accelerate launch --num_processes 8 main.py \
-  --model meta-llama/Meta-Llama-3-8B \
-  --tasks humaneval \
+  --model meta-llama/Llama-2-7b-hf \
+  --tasks mbppplus \
   --do_sample False \
   --n_samples 1 \
   --batch_size 1 \
@@ -51,10 +30,31 @@ accelerate launch --num_processes 8 main.py \
   --spr 0.5 \
   --widthtree $treesize \
   --check \
-  --kernelsize 16 \
+  --kernelsize 12 \
   --thr 0.05 \
 
 done 
+
+# treesizes=(1 4 6 8) 
+# for treesize in ${treesizes[@]} 
+# do 
+# accelerate launch --num_processes 8 main.py \
+#   --model meta-llama/Meta-Llama-3-8B \
+#   --tasks humaneval \
+#   --do_sample False \
+#   --n_samples 1 \
+#   --batch_size 1 \
+#   --max_length_generation 512 \
+#   --enable_epatches \
+#   --griffin \
+#   --allow_code_execution \
+#   --spr 0.5 \
+#   --widthtree $treesize \
+#   --check \
+#   --kernelsize 16 \
+#   --thr 0.05 \
+
+# done 
 
 # accelerate launch --num_processes 4 main.py \
 #   --model meta-llama/Meta-Llama-3-8B \
