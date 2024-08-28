fbf7ac7df1e9f726144464a7e5b5d587041d1816
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-08-28 19:00:08 -0400

    running"

--- runningscript.sh
+++ runningscript.sh
@@ -70,37 +70,36 @@ do
 #   --kernelsize 16 \
 #   --thr 0.05 \
 
-# accelerate launch --num_processes 4 main.py \
-#   --model meta-llama/Llama-2-13b-chat-hf \
-#   --tasks mbppplus \
-#   --do_sample False \
-#   --n_samples 1 \
-#   --batch_size 1 \
-#   --max_length_generation 512 \
-#   --enable_epatches \
-#   --allow_code_execution \
-#   --spr 0.5 \
-#   --kernelsize 12 \
-#   --thr 0.01 \
-#   --limit 100 \
-
-# accelerate launch --num_processes 4 main.py \
-#   --model meta-llama/Llama-2-13b-chat-hf \
-#   --tasks mbppplus \
-#   --do_sample False \
-#   --n_samples 1 \
-#   --batch_size 1 \
-#   --max_length_generation 512 \
-#   --enable_epatches \
-#   --griffin \
-#   --allow_code_execution \
-#   --spr 0.5 \
-#   --kernelsize 12 \
-#   --thr 0.01 \
-#   --limit 100 \
+accelerate launch --num_processes 4 main.py \
+  --model meta-llama/Llama-2-13b-hf \
+  --tasks mbppplus \
+  --do_sample False \
+  --n_samples 1 \
+  --batch_size 1 \
+  --max_length_generation 512 \
+  --enable_epatches \
+  --allow_code_execution \
+  --spr 0.5 \
+  --kernelsize 12 \
+  --limit 100 \
 
 accelerate launch --num_processes 4 main.py \
-  --model meta-llama/Meta-Llama-3-8B \
+  --model meta-llama/Llama-2-13b-hf \
+  --tasks mbppplus \
+  --do_sample False \
+  --n_samples 1 \
+  --batch_size 1 \
+  --max_length_generation 512 \
+  --enable_epatches \
+  --griffin \
+  --allow_code_execution \
+  --spr 0.5 \
+  --kernelsize 12 \
+  --thr 0.01 \
+  --limit 100 \
+
+accelerate launch --num_processes 4 main.py \
+  --model meta-llama/Llama-2-13b-hf \
   --tasks mbppplus \
   --do_sample False \
   --n_samples 1 \
@@ -112,7 +111,7 @@ accelerate launch --num_processes 4 main.py \
   --spr 0.5 \
   --widthtree $treesize \
   --check \
-  --kernelsize 16 \
+  --kernelsize 12 \
   --thr 0.05 \
 
 done 
